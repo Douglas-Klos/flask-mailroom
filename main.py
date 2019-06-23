e84f3053b92b9399207a787e1ebd387418b4f54b
Douglas Klos <dougklos@gmail.com>
2019-06-23 08:32:47 -0700

    recorrect overflow error since solution didn't work on heroku

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from model import Donation, Donor, User
 
 
 app = Flask(__name__)
-# app.secret_key = b"\x8d\xe5\xdf\x08L\xda<\x06S\xca\xab:\x8a\xee\xef\xfa\xfedV\xa84b\x06j\xd7N\xbf;\xe7\x174\x1a"
-app.secret_key = os.environ.get('SECRET_KEY').encode()
+app.secret_key = b"\x8d\xe5\xdf\x08L\xda<\x06S\xca\xab:\x8a\xee\xef\xfa\xfedV\xa84b\x06j\xd7N\xbf;\xe7\x174\x1a"
+# app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 
 @app.route("/")
@@ -38,19 +38,18 @@ def add_donation():
         return redirect(url_for("login"))
 
     if request.method == "POST":
-        try:
-            try:
-                existing_donor = Donor.get(Donor.name == request.form["donor"].title())
-                Donation(donor=existing_donor, value=request.form["value"]).save()
-            except DoesNotExist:
-                new_donor = Donor(name=request.form["donor"].title())
-                new_donor.save()
-                Donation(donor=new_donor, value=request.form["value"]).save()
-            return redirect(url_for("all_donations"))
-        except OverflowError:
+        if float(request.form["value"]) < 0 or float(request.form["value"]) > 10**10:
             return render_template(
                 "add.jinja2", error="Please enter a reasonable number..."
             )
+        try:
+            existing_donor = Donor.get(Donor.name == request.form["donor"].title())
+            Donation(donor=existing_donor, value=request.form["value"]).save()
+        except DoesNotExist:
+            new_donor = Donor(name=request.form["donor"].title())
+            new_donor.save()
+            Donation(donor=new_donor, value=request.form["value"]).save()
+        return redirect(url_for("all_donations"))
 
     return render_template("add.jinja2")
 
